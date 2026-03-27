1bcaaacc837be26d585deec687027b105545ee23
pkracht <1049314+pkracht@users.noreply.github.com>
2026-03-27 11:19:50 +0000

    chore(release): sync v1.0.3 (89201505)

--- scripts/mirror-registry.sh
+++ scripts/mirror-registry.sh
@@ -3,9 +3,9 @@
 cd "$( dirname -- "$( readlink -f -- "$0"; )"; )" || exit 1
 cd ..
 
-docker pull node:22.18.0-alpine3.21
-docker tag node:22.18.0-alpine3.21 registry.omega2k.de/library/node:22.18.0-alpine3.21
-docker push registry.omega2k.de/library/node:22.18.0-alpine3.21
+docker pull node:24.14.1-alpine3.22
+docker tag node:24.14.1-alpine3.22 registry.omega2k.de/library/node:24.14.1-alpine3.22
+docker push registry.omega2k.de/library/node:24.14.1-alpine3.22
 
 docker pull docker:28-cli
 docker tag docker:28-cli registry.omega2k.de/library/docker:28-cli
